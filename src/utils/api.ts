9eb9c7577deb5a31c0dc79631cb397a1b1ee790e
Simen Daehlin <simen.dehlin@gmail.com>
2021-08-14 08:01:40 +0100

    Added metrics

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 const APIKEY = process.env.APIKEY;
 
 export async function fetchOpenWeatherData(city: string): Promise<any> {
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},uk&APPID=${APIKEY}`;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},uk&units=metric&APPID=${APIKEY}`;
   const response = await fetch(url);
   if (!response.ok) throw new Error('City not found');
   const data = await response.json();
