c541611eb6e4cb7c7425c3c6d2ad7675fc9a86ee
Simen Daehlin <simen.dehlin@gmail.com>
2021-08-14 09:55:47 +0100

    Added eslint

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,23 +1,43 @@
 import React, { useState } from 'react';
 import '@fontsource/roboto';
 import ReactDOM from 'react-dom';
-import './popup.css';
+import { Box, InputBase, IconButton, Paper, Grid } from '@material-ui/core';
+import { Add as AddIcon } from '@material-ui/icons';
 import WeatherCard from './WeatherCard';
 import WeatherCardContainer from './WeatherCardContainer';
+import './popup.css';
 
 const App: React.FC<{}> = () => {
-  const [cities, setCities] = useState<string[]>([
-    'London',
-    'Bournemouth',
-    'Errori'
-  ]);
-  return (
-    <div>
-      {cities.map((city, index) => (
-        <WeatherCard city={city} key={index} />
-      ))}
-    </div>
-  );
+    const [cities, setCities] = useState<string[]>([
+        'London',
+        'Bournemouth',
+        'Errori'
+    ]);
+
+    const [cityInput, setCityInput] = useState<string>('');
+    return (
+        <Box mx={'8px'} my={'16px'}>
+            <Grid container>
+                <Grid item>
+                    <Paper>
+                        <Box px={'8px'} py={'16px'}>
+                            <InputBase
+                                placeholder="Add a city name"
+                                value={cityInput}
+                                onChange={(event) => setCityInput(event.target.value)}
+                            />
+                            <IconButton>
+                                <AddIcon></AddIcon>
+                            </IconButton>
+                        </Box>
+                    </Paper>
+                </Grid>
+            </Grid>
+            {cities.map((city, index) => (
+                <WeatherCard city={city} key={index} />
+            ))}
+        </Box>
+    );
 };
 
 const root = document.createElement('div');
